f22bcc85deff4789a721dba063f565055630bd4b
notstupidname <novikov.grigoriy@gmail.com>
2022-10-20 12:51:26 +0300

    1

--- assets/js/app.js
+++ assets/js/app.js
@@ -33,147 +33,23 @@
         }
     }, false);
 
-    // Carousel
-    function Carousel() {
-        const carouselEl = document.querySelector(".carousel");
-        const slideContainerEl = carouselEl.querySelector(".slider");
-        const slideEl = carouselEl.querySelector(".slide");
-        let slideWidth = slideEl.offsetWidth;
-        // Add click handlers
-        document.querySelector("#button_bck")
-            .addEventListener("click", () => navigate("backward"));
-        document.querySelector("#button_fwd")
-            .addEventListener("click", () => navigate("forward"));
-        
-        // Add keyboard handlers
-        document.addEventListener('keydown', (e) => {
-            if (e.code === 'ArrowLeft') {
-                navigate("backward");
-            } else if (e.code === 'ArrowRight') {
-                navigate("forward");
-            }
-        });
-        // Add resize handler
-        window.addEventListener('resize', () => {
-            slideWidth = slideEl.offsetWidth;
-        });
-
-        function changeSlideWidth() {
-            slideWidth = slideEl.offsetWidth;
-        }
-
-        const touch = matchMedia('(hover: none), (pointer: coarse)').matches;
-        if (!touch) {
-            new ResizeObserver(changeSlideWidth).observe(carouselEl);
-
-            slideContainerEl.addEventListener('click', (e) => {
-                e.preventDefault();
-                e.stopPropagation();
-                carouselEl.classList.toggle('big');
-            });
-        }
-
-        // Slide transition
-        const getNewScrollPosition = (arg) => {
-            const maxScrollLeft = slideContainerEl.scrollWidth - slideWidth;
-            if (arg === "forward") {
-                const x = slideContainerEl.scrollLeft + slideWidth;
-                return x <= maxScrollLeft ? x : 0;
-            } else if (arg === "backward") {
-                const x = slideContainerEl.scrollLeft - slideWidth;
-                return x >= 0 ? x : maxScrollLeft;
-            } else if (typeof arg === "number") {
-                const x = arg * (slideWidth);
-                return x;
-            }
-        }
-        const navigate = (arg) => {
-            slideContainerEl.scrollLeft = getNewScrollPosition(arg);
-        }
-
-    }
-    
     // Loading animation
     window.addEventListener('load', function() {
         body.classList.remove('is-loading');
         body.classList.remove('transition');
-        if (document.querySelector(".carousel")) {
-            Carousel();
-        }
-    });
-
-    // Popups open
-    const areaAll = document.querySelectorAll('.area');
-    for (const area of areaAll) {
-        area.addEventListener('click', function(e){
-            e.preventDefault();
-            e.stopPropagation();
-            const popup_id = this.dataset.popup;
-            const popupEl = document.getElementById(popup_id);
-            if (popupEl) {
-                popupEl.classList.remove("hide");
-                popupEl.classList.add("fade-in");
-            }
-            const overlayEl = document.querySelector('.global_overlay');
-            if (overlayEl) {
-                overlayEl.classList.remove("hide");
-                overlayEl.classList.add("fade-in");
-            }
-        })
-    }
-
-    // Popups close
-    const popupCloseAll = document.querySelectorAll('.popup_close');
-    for (const popupClose of popupCloseAll) {
-        popupClose.addEventListener('click', function(e){
-            e.preventDefault();
-            e.stopPropagation();
-            const popup_id_string = this.dataset.popup;
-            const popup_id_array = popup_id_string.split(' ');
-            for (const popup_id of popup_id_array) {
-                const popupEl = document.getElementById(popup_id);
-                if (popupEl) {
-                    popupEl.classList.remove("fade-in");
-                    popupEl.classList.add("fade-out");
-                    window.setTimeout(function() {
-                        popupEl.classList.add("hide");
-                        popupEl.classList.remove("fade-out");
-                    }, 450, popupEl);
+    
+        // Fade-in animation
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('show');
                 }
-            }
-            const overlayEl = document.querySelector('.global_overlay');
-            if (overlayEl) {
-                overlayEl.classList.remove("fade-in");
-                overlayEl.classList.add("fade-out");
-                window.setTimeout(function() {
-                    overlayEl.classList.add("hide");
-                    overlayEl.classList.remove("fade-out");
-                }, 450, overlayEl);
-            }
-        })
-    }
-
-    // Drawing animation
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('show');
-            }
+            });
         });
+
+        const hiddenDrawings = document.querySelectorAll('.hidden');
+        hiddenDrawings.forEach((el) => observer.observe(el));
     });
 
-    const hiddenDrawings = document.querySelectorAll('.hidden');
-    hiddenDrawings.forEach((el) => observer.observe(el));
-
-    // Grid change on logo
-
-    // const logo = document.querySelector('#header .logo');
-    // const projectsGrid = document.querySelector('.projects');
-    // logo.addEventListener('click', (e) => {
-    //     e.preventDefault();
-    //     e.stopPropagation();
-    //     projectsGrid.classList.toggle('simple-grid');
-    //     projectsGrid.classList.toggle('complex-grid');
-    // });
 
 })();
